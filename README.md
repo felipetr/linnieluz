5f67c343bc6ceb2a4b9bed3979216785e533294e
Felipe Travassos <contato@felipetravassos.com>
2024-11-06 01:19:01 -0300

    v0.2.0

--- README.md
+++ README.md
@@ -13,17 +13,17 @@ Dependências
 
 O tema utiliza as seguintes bibliotecas:
 
-*   [Montserrat Font Famlly](https://fonts.google.com/specimen/Montserrat)
+*   **[Montserrat Font Family](https://fonts.google.com/specimen/Montserrat)**  
     Adicionado via Google Fonts:
-
+    
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-*   [Bootstrap](https://getbootstrap.com/) - Versão: 5.3.3  
+    
+*   **[Bootstrap](https://getbootstrap.com/)** - Versão: 5.3.3  
     Adicionado via CDN:
     
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
-*   [Line Icons](https://lineicons.com/) - Versão: 5.0  
+*   **[Line Icons](https://lineicons.com/)** - Versão: 5.0  
     Adicionado via CDN:
     
         <link href="https://cdn.lineicons.com/5.0/lineicons.css" rel="stylesheet">
@@ -60,6 +60,15 @@ As seguintes dependências de desenvolvimento estão incluídas no projeto:
 *   `gulp-sourcemaps`: ^3.0.0
 *   `gulp-uglify`: ^3.0.2
 *   `sass`: ^1.77.6
+
+Como Gerar um Release
+---------------------
+
+Para criar um release e gerar um arquivo zipado da versão atual, use a task `gulp release` com o parâmetro `--ver` especificando o número da versão desejada:
+
+    gulp release --ver 0.1.2
+
+Essa task atualizará o número da versão no `package.json` e no `style.css`, criará um commit e tag no Git, e gerará um arquivo zip com o nome `linnieluz-v0.1.2.zip` na pasta `/release`.
 
 Licença
 -------
--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,16 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const fs = require('fs');
 const jsonminify = require('gulp-jsonminify');
-const zip = require('gulp-zip');
 const { exec } = require('child_process');
-gulp.task('release', (done) => {
+
+// Importação dinâmica para o gulp-zip (ESM)
+let zip;
+(async () => {
+    zip = (await import('gulp-zip')).default;
+})();
+
+// Task para liberar uma nova versão do projeto
+gulp.task('release', async (done) => {
     const newVersion = process.argv[4];
 
     if (!newVersion) {
@@ -18,61 +25,40 @@ gulp.task('release', (done) => {
         return;
     }
 
+    // Atualiza a versão no package.json
+    console.log('Atualizando o package.json ...');
     const packageJson = JSON.parse(fs.readFileSync('./package.json'));
     packageJson.version = newVersion;
     fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
 
+    // Atualiza a versão no style.css
+    console.log('Atualizando o style.css ...');
     const styleCssPath = './dist/style.css';
     let styleCssContent = fs.readFileSync(styleCssPath, 'utf8');
     styleCssContent = styleCssContent.replace(/Version: .*/g, `Version: ${newVersion}`);
     fs.writeFileSync(styleCssPath, styleCssContent);
 
+    // Commit e tag no Git
+    console.log('Gerando tag git...');
     exec(`npm install && git add . && git commit -m "v${newVersion}" && git tag v${newVersion} && git push && git push origin v${newVersion}`, (err, stdout, stderr) => {
         if (err) {
             console.error(`Erro ao executar comandos git: ${stderr}`);
             done(err);
             return;
         }
+        
         console.log(stdout);
-        done();
-    });
 
-    gulp.task('release', (done) => {
-        const newVersion = process.argv[4];
-    
-        if (!newVersion) {
-            console.error('Erro: Especifique o número da versão. Exemplo: gulp release --ver 0.1.1');
-            done();
-            return;
-        }
-    
-        console.log('Atualizando o package.json ...');
-        const packageJson = JSON.parse(fs.readFileSync('./package.json'));
-        packageJson.version = newVersion;
-        fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
-        console.log('Atualizando o style.css ...');
-        const styleCssPath = './dist/style.css';
-        let styleCssContent = fs.readFileSync(styleCssPath, 'utf8');
-        styleCssContent = styleCssContent.replace(/Version: .*/g, `Version: ${newVersion}`);
-        fs.writeFileSync(styleCssPath, styleCssContent);
-        console.log('Gerando tag git...');
-        exec(`npm install && git add . && git commit -m "v${newVersion}" && git tag v${newVersion} && git push && git push origin v${newVersion}`, (err, stdout, stderr) => {
-            if (err) {
-                console.error(`Erro ao executar comandos git: ${stderr}`);
-                done(err);
-                return;
-            }
-            
-            console.log(stdout);
-            console.log(`Gerando arquivo linnieluz-v${newVersion}.zip ...`);
-            gulp.src('./dist/**/*')
-                .pipe(zip(`linnieluz-v${newVersion}.zip`))
-                .pipe(gulp.dest('./release'))
-                .on('end', done);
-        });
+        // Cria o arquivo zip para o release
+        console.log(`Gerando arquivo linnieluz-v${newVersion}.zip ...`);
+        gulp.src('./dist/**/*')
+            .pipe(zip(`linnieluz-v${newVersion}.zip`))
+            .pipe(gulp.dest('./release'))
+            .on('end', done);
     });
 });
 
+// Task para compilar os estilos
 gulp.task('styles', () => {
     return gulp.src('./src/sass/**/*.scss')
         .pipe(sourcemaps.init())
@@ -82,6 +68,7 @@ gulp.task('styles', () => {
         .pipe(gulp.dest('./dist/assets/css'));
 });
 
+// Task para minificar scripts
 gulp.task('scripts', () => {
     return gulp.src('./src/js/**/*.js')
         .pipe(sourcemaps.init())
@@ -89,15 +76,20 @@ gulp.task('scripts', () => {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/assets/js'));
 });
+
+// Task para minificar JSON
 gulp.task('json', () => {
     return gulp.src('./src/json/**/*.json')
         .pipe(jsonminify())
         .pipe(gulp.dest('./dist/assets/json'));
 });
+
+// Observador de mudanças
 gulp.task('watch', () => {
     gulp.watch('./src/sass/**/*.scss', gulp.series('styles'));
     gulp.watch('./src/js/**/*.js', gulp.series('scripts'));
     gulp.watch('./src/json/**/*.json', gulp.series('json'));
 });
 
+// Task padrão
 gulp.task('default', gulp.series('styles', 'scripts', 'watch'));
